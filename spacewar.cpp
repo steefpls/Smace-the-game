f1146cddfebb3054b40f8b9c71a01267b2f19e61
BuildTools <unconfigured@null.spigotmc.org>
2018-11-26 11:56:12 +0800

    Why

--- spacewar.cpp
+++ spacewar.cpp
@@ -52,8 +52,8 @@ void Spacewar::initialize(HWND hwnd)
 		throw(GameError(gameErrorNS::FATAL_ERROR, "Error initializing wall"));
 	wall1.setFrames(wallNS::WALL1_START_FRAME, wallNS::WALL1_END_FRAME);
 	wall1.setCurrentFrame(wallNS::WALL1_START_FRAME);
-	wall1.setX(0*wallNS::WIDTH);
-	wall1.setY(0);
+	wall1.setX(200);
+	wall1.setY(200);
 	
 	
 	//ship1.setVelocity(VECTOR2(shipNS::SPEED, -shipNS::SPEED)); // VECTOR2(X, Y)
@@ -100,9 +100,9 @@ void Spacewar::update()
 {
 	//bullet1.update(frameTime); //update bullet frames
 
-	ship1.update(frameTime); //update ship frames
-	wall1.update(frameTime);
-	//collide with right wall
+	ship1.update(frameTime);	//update ship frames
+	wall1.update(frameTime);	//update wall frames
+								
 
 	if (input->isKeyDown(ESC_KEY))
 	{
@@ -119,20 +119,13 @@ void Spacewar::update()
 void Spacewar::collisions()
 {
 	VECTOR2 collisionVector;
+
 	if (ship1.collidesWith(wall1, collisionVector))
 	{
-		//collide with right wall
-		if (ship1.getX() + shipNS::WIDTH > (wall1.getX()))
-		{
-			ship1.setX(wall1.getX());
-		}
-	
-		//collide with left wall
-		else if (ship1.getX() < wall1.getX() + wallNS::WIDTH)
-		{
-			ship1.setX(wall1.getX() + wallNS::WIDTH);
-		}
+		ship1.setY(900);
 	}
+
+	
 	//// if collision between ships
 	//if (ship1.collidesWith(ship2, collisionVector))
 	//{
--- wall.cpp
+++ wall.cpp
@@ -8,7 +8,7 @@ Wall::Wall() : Entity()
 {
 	spriteData.width = wallNS::WIDTH;	//size of wall
 	spriteData.height = wallNS::HEIGHT;
-	radius = wallNS::WIDTH/2;
+	//radius = wallNS::WIDTH/2;
 	spriteData.x = wallNS::X;
 	spriteData.y = wallNS::Y;
 	mass = wallNS::MASS;
