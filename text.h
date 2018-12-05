77eb764675e3a6c40db1b4ca496801fb509208ae
WSH <supersaihong69@gmail.com>
2018-12-06 05:10:29 +0800

    minor change

--- text.h
+++ text.h
@@ -1,7 +1,4 @@
-//  Module:             Gameplay Programming
-//  Assignment1:        Ships and Shapes
-//  Student Name:       Steven Koe
-//  Student Number:     S10181536D
+
 
 #ifndef _TEXTDX_H               // Prevent multiple definitions if this 
 #define _TEXTDX_H               // file is included in more than one place
