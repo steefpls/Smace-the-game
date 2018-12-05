19d895c729c6022ddb8bce0253c544b36446d542
WSH <supersaihong69@gmail.com>
2018-12-05 10:18:22 +0800

    MORE COLLISIONS

--- spacewar.cpp
+++ spacewar.cpp
@@ -578,12 +578,35 @@ void Spacewar::collisions()
 			SAFE_DELETE(ship2.bulletList[i]);
 			ship2.bulletList.erase(ship2.bulletList.begin() + i);
 		}
+
+		for (int j = 0; j < (ship1.missileList.size()); j++)
+		{
+			if (ship2.bulletList[i]->collidesWith(*ship1.missileList[j], collisionVector))
+			{
+				explosionList.push_back(new Explosion);
+				explosionList[explosionList.size() - 1]->initialize(this, explosionNS::WIDTH, explosionNS::HEIGHT, explosionNS::TEXTURE_COLS, &explosionTexture);
+				explosionList[explosionList.size() - 1]->setFrames(explosionNS::EXPLOSION_START_FRAME, explosionNS::EXPLOSION_END_FRAME);
+				explosionList[explosionList.size() - 1]->setX(ship1.missileList[j]->getCenterX() - explosionList[explosionList.size() - 1]->getWidth());
+				explosionList[explosionList.size() - 1]->setY(ship1.missileList[j]->getCenterY() - explosionList[explosionList.size() - 1]->getHeight());
+				SAFE_DELETE(ship1.missileList[j], collisionVector);
+				ship1.missileList.erase(ship1.missileList.begin() + j);
+			}
+		}
 	}
 
 	//MISSILE COLLISION
-	for (int i = 0; i < (ship2.missileList.size()); i++)
+	for (int i = 0; i < (ship1.missileList.size()); i++)
 	{
-
+		if (ship1.missileList[i]->collidesWith(ship2,collisionVector))
+		{
+			explosionList.push_back(new Explosion);
+			explosionList[explosionList.size() - 1]->initialize(this, explosionNS::WIDTH, explosionNS::HEIGHT, explosionNS::TEXTURE_COLS, &explosionTexture);
+			explosionList[explosionList.size() - 1]->setFrames(explosionNS::EXPLOSION_START_FRAME, explosionNS::EXPLOSION_END_FRAME);
+			explosionList[explosionList.size() - 1]->setX(ship1.missileList[i]->getCenterX() - explosionList[explosionList.size() - 1]->getWidth());
+			explosionList[explosionList.size() - 1]->setY(ship1.missileList[i]->getCenterY() - explosionList[explosionList.size() - 1]->getHeight());
+			SAFE_DELETE(ship1.missileList[i]);
+			ship1.missileList.erase(ship1.missileList.begin() + i);
+		}
 	}
 	//if (ship1.collidesWith(missile1, collisionVector))
 	//{
